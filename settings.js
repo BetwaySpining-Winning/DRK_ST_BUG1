23353c610aa43ca2d7b75f9913042ef8112e3330
BetwaySpining-Winning <alihhmuhhamed@gmail.com>
2024-09-24 12:01:05 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,15 +2,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "50931461936"
+global.ownernomer = "27728653929"
 global.ownername = "🌹⃟⃢👑 ĐÆ𝚪𝐊༒𝙎-𝙏𝞢𝞜🌹⃟⃢👑"
 global.ytname = "DRK_ST_Tech"
 global.socialm = "GitHub: DRK-S-TEN"
 global.location = "America, Haiti, Port-au-Prince"
 
-global.ownernumber = '50931461936'  //creator number
+global.ownernumber = '27728653929'  //creator number
 global.ownername = '𝙎-𝙏𝞢𝞜' //owner name
-global.botname = 'DRK_ST_BUG' //name of the bot
+global.botname = 'NHLAKZIN_BOT' //name of the bot
 
 //sticker details
 global.packname = '🗾'
